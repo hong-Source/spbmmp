77cee8b76a5d36e740582602c82010e8440c9771
hongyan <454651895@qq.com>
2019-03-18 00:34:43 +0800

    test

--- api/api_system/src/main/java/com/hong/api/system/controller/SystemDepartmentController.java
+++ api/api_system/src/main/java/com/hong/api/system/controller/SystemDepartmentController.java
@@ -2,6 +2,7 @@ package com.hong.api.system.controller;
 
 import com.hong.dao.system.SystemDepartmentMapper;
 import com.hong.model.system.SystemDepartment;
+import com.hong.service.system.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,9 @@ import java.util.List;
 @RequestMapping("/depart")
 public class SystemDepartmentController {
     @Autowired
-    private SystemDepartmentMapper departmentMapper;
+    private DepartmentService departmentService;
     @RequestMapping("/getall")
     public List<SystemDepartment> getAll(){
-        return departmentMapper.findAll();
+        return departmentService.getAll();
     }
 }
--- service/src/main/java/com/hong/service/system/impl/DepartmentServiceImpl.java
+++ service/src/main/java/com/hong/service/system/impl/DepartmentServiceImpl.java
@@ -4,9 +4,11 @@ import com.hong.dao.system.SystemDepartmentMapper;
 import com.hong.model.system.SystemDepartment;
 import com.hong.service.system.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class DepartmentServiceImpl implements DepartmentService{
 
     @Autowired
