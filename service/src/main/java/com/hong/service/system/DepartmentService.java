a47326e82ca9589290c5f606ff61a7dd384043dd
guxiansheng <454651895@qq.com>
2019-03-18 18:20:25 +0800

    有bug，无奈

--- service/src/main/java/com/hong/service/system/DepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.hong.service.system;
-
-import com.hong.model.system.SystemDepartment;
-
-import java.util.List;
-
-public interface DepartmentService {
-    List<SystemDepartment> getAll();
-}
--- service/src/main/java/com/hong/service/system/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.hong.service.system;
-
-import com.hong.model.system.SystemUser;
-
-import java.util.List;
-
-public interface UserService {
-    List<SystemUser> getAll();
-}
